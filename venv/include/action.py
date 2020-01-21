b608378e5b68f9360acfbd4dda7988a94a425c61
Eli Zat <eli.zatlawy@gmail.com>
2020-01-21 22:41:31 +0200

    fixed the delete db function

--- venv/include/action.py
+++ venv/include/action.py
@@ -10,12 +10,12 @@ def update_db(input_file):
         for line in file:
             data = line.split(', ')
             curr_product = repo.Products.find(id=data[0])[0]
-            if int(data[1]) < 0:
+            if int(data[1]) < 0: # it is a sale
                 if curr_product.quantity >= int(data[1]):
                     repo.Products.update_quantity(data[0], data[1])
                     curr_act = Activitie(data[0], data[1], data[2], data[3])
                     repo.Activities.insert(curr_act)
-            else:
+            else: # it is a supply 
                 repo.Products.update_quantity(data[0], data[1])
                 curr_act = Activitie(data[0], data[1], data[2], data[3])
                 repo.Activities.insert(curr_act)
--- venv/include/printdb.py
+++ venv/include/printdb.py
@@ -11,22 +11,22 @@ def print_db():
         activity_tuple = (activity.product_id, activity.quantity, activity.activator_id, activity.date)
         print(activity_tuple)
     print("Coffee stands")
-    for stand in repo.Coffee_stands.find_all():
+    for stand in repo.Coffee_stands.find_all_ordered_by('id'):
         stand_tuple = (stand.id, stand.location, stand.number_of_employees)
         print(stand_tuple)
     print("Employees")
-    for employee in repo.Employees.find_all():
+    for employee in repo.Employees.find_all_ordered_by('id'):
         employee_tuple = (employee.id, employee.name, employee.salary, employee.coffee_stand)
         print(employee_tuple)
     print("Products")
-    for product in repo.Products.find_all():
+    for product in repo.Products.find_all_ordered_by('id'):
         product_tuple = (product.id, product.description, product.price, product.quantity)
         print(product_tuple)
     print("Suppliers")
-    for supplier in repo.Suppliers.find_all():
+    for supplier in repo.Suppliers.find_all_ordered_by('id'):
         supplier_tuple = (supplier.id, supplier.name, supplier.contact_information)
         print(supplier_tuple)
-
+    print()
     employees = repo.Employees.find_all_ordered_by('name')
     # print the Employees report if there are Employees
     if employees is not None:
@@ -37,8 +37,8 @@ def print_db():
                 if (int(activity.quantity) < 0) and (activity.activator_id == employee.id):
                     curr_product = repo.Products.find(id=activity.product_id)[0]
                     total_sales = total_sales + (-activity.quantity * curr_product.price)
-            employee_tuple = (employee.id, employee.name, employee.salary, total_sales)
-            print(employee_tuple)
+            bld_name = repo.Coffee_stands.find(id=employee.coffee_stand)[0].location
+            print(employee.name, employee.salary, bld_name, total_sales)
     # print the Activities report if there are activities
     cursor = repo._conn.cursor()
     cursor.execute("""SELECT Activities.date, Products.description, Activities.quantity, Employees.name, Suppliers.name 
@@ -48,6 +48,7 @@ def print_db():
                               ORDER BY Activities.date ASC """)
     activities_tuple = cursor.fetchall()
     if len(activities_tuple) > 0:
+        print()
         print("Activities")
         for activity in activities_tuple:
             print(activity)
